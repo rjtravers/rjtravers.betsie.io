eb082e14d9c1be237e4020696bf27ee6e1385d73
rjtravers <38090110+rjtravers@users.noreply.github.com>
2024-10-16 21:17:27 -0400

    Corrected console.log syntax error

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 window.onload = function() {
           alert("The page has fully loaded!");
-          console.log("Here's where the logging goes!"
+          console.log("Here's where the logging goes!")
       };

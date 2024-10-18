9991b4cdb279d138fc29c11240433576306244a9
rjtravers <38090110+rjtravers@users.noreply.github.com>
2024-10-17 22:04:23 -0400

    Removed fetch function

--- site/script.js
+++ site/script.js
@@ -1,10 +1,3 @@
 window.onload = function() {
           console.log("This is coming from site/script.js");
 };
-
-fetch('/rjtravers.betsie.io/header/index.html')  // Add your actual repository name here
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading header:', error));

7fce6811fc0a6d866da4e75468a553e9c3ba8438
rjtravers <38090110+rjtravers@users.noreply.github.com>
2024-10-17 21:17:23 -0400

    Add fetch to import header row

--- site/script.js
+++ site/script.js
@@ -1,3 +1,9 @@
 window.onload = function() {
           console.log("This is coming from site/script.js");
-      };
+};
+
+fetch('header/index.html')
+          .then(response => response.text())
+          .then(data => {
+                    document.getElementById('header').innerHTML = data;
+});

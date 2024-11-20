d29eaa1c7ac1738be05c04c223f9b1e7828c9ddc
rjtravers <38090110+rjtravers@users.noreply.github.com>
2024-11-19 20:47:11 -0500

    Update script.js with more console testing

--- findLine/script.js
+++ findLine/script.js
@@ -2,8 +2,8 @@ window.onload = function() {
           console.log("This is the Find a line page. The window has loaded!");
       };
 
-DOMContentLoaded = function() {
-          console.log("The DOM content has loaded!");
-};
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("The DOM content has loaded!");
+});
 
 console.log("This is outside the window.onload action");
